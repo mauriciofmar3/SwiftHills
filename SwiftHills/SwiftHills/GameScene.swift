468d43e00e5630b4e5b360dad32e870e47c1eaf7
Mauricio E. Bell'Albero <mauricio.bellalbero@gmail.com>
2015-03-04 00:49:25 -0800

    Removed old boilerplate code

--- SwiftHills/SwiftHills/GameScene.swift
+++ SwiftHills/SwiftHills/GameScene.swift
@@ -14,13 +14,6 @@ class GameScene: SKScene {
     var parallaxNodeBackgrounds : ParallaxNode!
     
     override func didMoveToView(view: SKView) {
-        /* Setup your scene here */
-//        let myLabel = SKLabelNode(fontNamed:"Chalkduster")
-//        myLabel.text = "Hello, World!";
-//        myLabel.fontSize = 65;
-//        myLabel.position = CGPoint(x:CGRectGetMidX(self.frame), y:CGRectGetMidY(self.frame));
-//        
-        //        self.addChild(myLabel)
         self.backgroundColor = SKColor.blackColor()
 
         self.setupBackground()
@@ -48,23 +41,7 @@ class GameScene: SKScene {
     }
     
     override func touchesBegan(touches: NSSet, withEvent event: UIEvent) {
-        /* Called when a touch begins */
         
-        for touch: AnyObject in touches {
-            let location = touch.locationInNode(self)
-            
-            let sprite = SKSpriteNode(imageNamed:"Spaceship")
-            
-            sprite.xScale = 0.5
-            sprite.yScale = 0.5
-            sprite.position = location
-            
-            let action = SKAction.rotateByAngle(CGFloat(M_PI), duration:1)
-            
-            sprite.runAction(SKAction.repeatActionForever(action))
-            
-            self.addChild(sprite)
-        }
     }
    
     override func update(currentTime: CFTimeInterval) {
