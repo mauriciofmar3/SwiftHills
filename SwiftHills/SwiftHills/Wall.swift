3ab8da41f4ee322c82cae9987ed9b59eae8fa243
Mauricio E. Bell'Albero <mauriciofmar3@gmail.com>
2015-03-07 17:27:44 -0800

    required initializer added to wall

--- SwiftHills/SwiftHills/Wall.swift
+++ SwiftHills/SwiftHills/Wall.swift
@@ -13,4 +13,8 @@ class Wall: SKNode {
     override init() {
         super.init()
     }
+
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
 }
